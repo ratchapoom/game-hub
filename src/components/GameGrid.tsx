d21a37f4e5d5a59215daa6d6fbdc41df695d272b
ratchapoom <ratchapoom09@gmail.com>
2023-10-25 17:35:14 +0700

    fliter game by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,9 +3,14 @@ import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GamecardContainer from "./GamecardContainer";
+import { Genre } from "../hooks/useGenres";
 
-const GameGrid = () => {
-  const { data, error, isLoading } = useGames();
+interface Props {
+  selectedGenre: Genre | null;
+}
+
+const GameGrid = ({ selectedGenre }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
@@ -18,13 +23,13 @@ const GameGrid = () => {
       >
         {isLoading &&
           skeletons.map((skeleton) => (
-            <GamecardContainer>
-              <GameCardSkeleton key={skeleton} />
+            <GamecardContainer key={skeleton}>
+              <GameCardSkeleton />
             </GamecardContainer>
           ))}
         {data?.map((data) => (
-          <GamecardContainer>
-            <GameCard key={data.id} game={data} />
+          <GamecardContainer key={data.id}>
+            <GameCard game={data} />
           </GamecardContainer>
         ))}
       </SimpleGrid>
