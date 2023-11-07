3a7b513384302b23bc2ae8365df7f23462985a2f
ratchapoom <ratchapoom09@gmail.com>
2023-11-08 01:49:35 +0700

    handle game with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from '../assets/_Gift Voucher 329 (1040 × 1040 px) (22).png'
+
 
 const getCroppedImageUrl = (url : string) => {
-    if(!url) return ''
+    if(!url) return noImage
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
